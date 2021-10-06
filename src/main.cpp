550a5772911b4934df9c635030c61ce4628b3fda
croc_o_beans <harryrhodes2005@gmail.com>
2021-10-06 17:48:58 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 //modify these
-int timeout = 3500; //time to fail in ms / 5.7
-//int beepInterval = 1000; // buzzer interval(in ms)
+int timeout = 20000;                          //time to fail in ms
+int beepInterval = 1000;                      // buzzer interval(in ms)
 
 bool t = false ;
 bool gameState = false;
@@ -9,29 +9,31 @@ bool gameState = false;
 
 void setup() {
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);               //set pins as output
 
   for(int i =2 ; i<=8 ; i++){
     pinMode(i, OUTPUT);    
   }
 
-  digitalWrite(8,LOW);
+  digitalWrite(8,LOW);                        //make sure the buzzer isnt on 
 
+  beepInterval = beepInterval/5;
 
 }
 
 void loop() {
-  if(gameState == false){
+
+  if(gameState == false){                      //init game
 
     t = false;
     
     digitalWrite(LED_BUILTIN, HIGH);
 
-    for(int i=2; i <= 7 ; i++){
+    for(int i=2; i <= 7 ; i++){               // turn on all LEDs
       digitalWrite(i , HIGH);
     }
 
-    while(t == false){
+    while(t == false){                        //hang program until a button is pressed
       for(int i = 2; i<=7; i++){
         if(analogRead(i)>500){
           t = true;
@@ -40,7 +42,7 @@ void loop() {
     }
 
     for(int i=2; i <= 7 ; i++){
-      digitalWrite(i , LOW);
+      digitalWrite(i , LOW);                    // turn off all LEDs
     }
 
     gameState = true;
@@ -52,36 +54,40 @@ void loop() {
   if(gameState == true){
 
     int count = 0;
-//    bool buzzOn = false;
+    int buzzOn = 0;
 
-    int lightNum = random(2,8);
+    int lightNum = random(2,8);                   //generate rand
 
     digitalWrite(lightNum,HIGH);
 
-    while( analogRead(lightNum) < 500 ){
+    while( analogRead(lightNum) < 500 ){          //hang program until light is pressed
       count +=1  ;
-//      if(0 == count % beepInterval){
-//        if(buzzOn == true){
-//          digitalWrite(8,LOW);
-//          buzzOn = false;
-//        }
-//        else{
-//          digitalWrite(8,HIGH);
-//          buzzOn = true;
-//        }
-        
-//      }
-      
-      if(count > timeout){
+
+
+      if(0 == count % beepInterval){              // increment timer state if counter mod beepinterval =0
+        if(buzzOn >3){
+          digitalWrite(8,HIGH);
+          buzzOn = 0;
+        }
+        else{
+          digitalWrite(8,LOW);
+          buzzOn ++;
+        }
+       
+      }
+
+      if(count > timeout){                        // if over timeout limit return to init
         gameState = false;
         break;
       }
 
-//      digitalWrite(8,LOW);
-
+      delay(1);  
+      
     }
 
     digitalWrite(lightNum,LOW);
+    digitalWrite(8,LOW);                          //make sure buzzer is off
+
   }
   
 }
